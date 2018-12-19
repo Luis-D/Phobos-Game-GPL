84804da1ff0996ae44de6a91a5a6d97146ae1822
Luis-D <luisnestordl@otmail.com>
2018-12-19 01:30:36 -0400

    Update

--- script.lua
+++ script.lua
@@ -18,8 +18,8 @@ Jugador = Entity_Create(0,4,0,0,0.1,10,0.25,nil,nil,Model,CharaSize);
 --[[Load NPCs]]--
 for i=2,MaxInstances do
     Model = VRAM_Instance_Create(i,Character_model);
-    NPC = Entity_Create(i-1,-2.3+(i*0.05),0,-90+i,0.04,5,0.15,Script_This(),"stalk",Model,CharaSize);
-    Entity_Set_Sensor(NPC,80,7)
+    NPC = Entity_Create(i-1,-2.3+(i*0.05),0,0+i,0.04,5,0.15,Script_This(),"stalk",Model,CharaSize);
+    Entity_Set_Sensor(NPC,90,6)
 end
 
 --[[Load World]]--
@@ -27,7 +27,6 @@ Scene_Set_Map("Lab1.stl");
 Scene_Set_Script(Script_This());
 Camera_add(-4,14,3,-4,0,0,60,14);
 Camera_add(9,9,3,6,6,0,60,10);
---Camera_add(-2,-12,3, 0,0,0,60,14);
 Camera_add(1, -12,3,3,0,0,45,16);
 Camera_add(-4, -13,3,-3,0,0,45,16);
 
@@ -49,9 +48,13 @@ VRAM_Buffer_fill(1,1,MaxInstances,0);
 function stalk (caller)
     Entity_Path_Clear(caller);
     v=Entity_Sensor_Check_Entity(caller,Jugador)
-    if(1==0) then
-	Entity_Movement(caller,0,0);
-    else
+	io.write(Entity_Get_ID(caller) .. ": ");
+	print(v)
+	Speed = 0.2;
+    if(v) then
+	Speed=1
+	end
+    
 	PP = Entity_Get_Position(Jugador);
 	CalP= Entity_Get_Position(caller);
 	DD = V2Distance(PP,CalP);
@@ -60,10 +63,10 @@ function stalk (caller)
 	PATH = Path_Find(Node,Node2,0,PP);
 	Entity_Path_Set(caller,PATH);
 	X,Y = Entity_Path_Get_XY(caller);
-	if Entity_Goto_EXT(caller,X,Y,1,1) then
+	if Entity_Goto_EXT(caller,X,Y,Speed,1) then
 		Entity_Movement(caller,0,0);
     	end
-    end
+    
 end
 
 function TP(Caller,Parameter)
